85fabf5f11c606d2090b21d40449570b685d7089
junhee916 <junhee916@naver.com>
2022-03-09 21:17:46 +0900

    set board node and mongodb

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const userRouter = require('./routes/user')
+const boardRouter = require('./routes/board')
 const connectDB = require('./config/database')
 connectDB()
 // middleware
@@ -12,5 +13,6 @@ app.use(bodyParser.urlencoded({extended : false}))
 app.use(morgan('dev'))
 // router
 app.use('/user', userRouter)
+app.use('/board', boardRouter)
 const PORT = process.env.PORT || 7000
 app.listen(PORT, console.log("connected server..."))
